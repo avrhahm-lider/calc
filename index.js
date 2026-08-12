5933206c95e4a2cc4414bdf149e04cebe548eb98
avrhahm <am500148148@gmail.com>
2026-08-12 12:08:00 +0300

    .

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log('hello');
+// console.log(document.querySelector('h1').outerText);
+
+
+// let title = document.querySelector('h1')
+// console.log(title.textContent);
+
+// let d = document.getElementById("myInput")
+
+// for(let i =0; i<20; i++){
+//     d.value += d.value = "נהוראי מגורי"
+// }
+
+const screnVal = document.getElementById('scren')
+
+function calcfunc(sec){
+    screnVal.value += sec.textContent
+    console.log(sec.textContent);
+    
+}
+
+function deletefunc(){
+    screnVal.value =""
+}
+
+function equalfunc(){
+    try{
+    screnVal.value =  eval(screnVal.value)
+    }catch{
+        screnVal.value = "error!"
+    }
+}
